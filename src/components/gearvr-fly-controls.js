4948f31281b35f59659b25fcaa6684c85f313862
Charlie <charlie.hoey@gmail.com>
2017-03-20 12:57:45 -0400

    updates to gearvr controls to allow for forward and backward motion

--- src/components/gearvr-fly-controls.js
+++ src/components/gearvr-fly-controls.js
@@ -1,7 +1,8 @@
 /* globals AFRAME THREE */
 AFRAME.registerComponent('gearvr-fly-controls', {
   schema: {
-    active: {type: 'boolean', default: false}
+    active: {type: 'boolean', default: false},
+    direction: {type: 'float', default: 0.0}
   },
 
   init: function () {
@@ -9,8 +10,12 @@ AFRAME.registerComponent('gearvr-fly-controls', {
     this.handleControlChange = this.handleControlChange.bind(this);
 
     this.hand = document.getElementById('right-hand');
-    this.hand.addEventListener('trackpadup', (evt) => { this.handleControlChange(false, evt) });
-    this.hand.addEventListener('trackpaddown', (evt) => { this.handleControlChange(true, evt) });
+    // this.hand.addEventListener('trackpadup', (evt) => { this.handleControlChange(false, evt) });
+    // this.hand.addEventListener('trackpaddown', (evt) => { this.handleControlChange(true, evt) });
+
+    this.hand.addEventListener('axismove', (evt) => {
+      this.handleControlChange(evt.detail.axis[0], evt);
+    })
 
     this.cursor = document.getElementById('acursor');
 
@@ -20,9 +25,10 @@ AFRAME.registerComponent('gearvr-fly-controls', {
   },
 
   handleControlChange(state, evt) {
-    this.data.active = state;
+    this.data.active = state !== 0;
+    this.data.direction = -state;
     this.cursor.setAttribute('material', 'color', state ? "#ff6600" : "#ffffff" );
-    console.log(evt);
+    // console.log(evt.detail.axis);
   },
 
   update: function () {
@@ -33,7 +39,7 @@ AFRAME.registerComponent('gearvr-fly-controls', {
     if(this.data.active) {
       this.direction.set(0,0,1);
       this.direction.applyQuaternion(this.el.object3D.quaternion);
-      this.el.object3D.position.sub(this.direction.multiplyScalar(this.speed));
+      this.el.object3D.position.sub(this.direction.multiplyScalar(this.speed*this.data.direction));
       let p = this.el.object3D.position;
       this.el.setAttribute('position', `${p.x} ${p.y} ${p.z}`);
     }
--- src/glsl/starfield.vert
+++ src/glsl/starfield.vert
@@ -11,7 +11,5 @@ void main() {
   vUv = uv;
   starColor = blackbody(ci / 10000.);
   gl_PointSize = max(1.0, 15.0/distance(position, cameraPosition));
-  // gl_PointSize *= (1.0 - absmag)
-  // gl_PointSize *= max(1.0, (1.44 + max(-1.0, absmag/55.0)));
   gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 }
